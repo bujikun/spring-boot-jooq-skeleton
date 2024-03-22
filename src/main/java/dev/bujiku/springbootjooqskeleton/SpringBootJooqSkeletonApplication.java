147561684b4472c283b1d2569723e9c7dca1de2c
Newton Bujiku <bujikun@vodacom.co.tz>
2024-03-22 16:05:04 +0300

    Add dependencies and configuration for redis-om & mapstruct

--- src/main/java/dev/bujiku/springbootjooqskeleton/SpringBootJooqSkeletonApplication.java
+++ src/main/java/dev/bujiku/springbootjooqskeleton/SpringBootJooqSkeletonApplication.java
@@ -1,9 +1,11 @@
 package dev.bujiku.springbootjooqskeleton;
 
+import com.redis.om.spring.annotations.EnableRedisDocumentRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRedisDocumentRepositories
 public class SpringBootJooqSkeletonApplication {
 
 	public static void main(String[] args) {

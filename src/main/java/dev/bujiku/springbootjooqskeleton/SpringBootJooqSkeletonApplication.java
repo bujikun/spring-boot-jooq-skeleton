16fa5c1372a6a2d3b5ab66aafdec5988a364134d
Newton Bujiku <bujikun@vodacom.co.tz>
2024-03-22 14:32:17 +0300

    Initial commit

---
+++ src/main/java/dev/bujiku/springbootjooqskeleton/SpringBootJooqSkeletonApplication.java
@@ -0,0 +1,13 @@
+package dev.bujiku.springbootjooqskeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJooqSkeletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJooqSkeletonApplication.class, args);
+	}
+
+}
